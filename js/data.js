ed861dd02ee3f91b2cf0e8096094c4a1fb10661a
ieva Skyriene <ieva.skyriene@gmail.com>
2020-07-10 15:27:21 +0300

    initial

---
+++ js/data.js
@@ -0,0 +1,32 @@
+/** @format */
+
+"use strict";
+
+const navigation = [
+  {
+    ref: "Picos",
+  },
+  {
+    ref: "#about",
+  },
+  {
+    ref: "#portfolio",
+    text: "Portfolio",
+    class: "ref",
+  },
+  {
+    ref: "#services",
+    text: "Services",
+    class: "ref",
+  },
+  {
+    ref: "#blog",
+    text: "Blog",
+    class: "ref",
+  },
+  {
+    ref: "#contact",
+    text: "Contact",
+    class: "ref",
+  },
+];
